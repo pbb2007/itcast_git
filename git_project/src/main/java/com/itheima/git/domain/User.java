42a3feebf8c9bc77ab1738ffaa0faf021fc6b422
pbb <742819148@qq.com>
2019-01-04 15:20:44 +0800

    新建User.java，提交到本地仓库

---
+++ git_project/src/main/java/com/itheima/git/domain/User.java
@@ -0,0 +1,27 @@
+package com.itheima.git.domain;
+
+/**
+ * @Author: pbb
+ * @Date: 2019/1/4 15:12
+ * @Version 1.0
+ */
+public class User {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
